6016727b15217266796c8680482b3074092d77c2
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2022-03-05 11:45:00 +0100

    feat(hkt): add support of higher-kinded-types
    The implementation of HKT is taken form [mikearnaldi/hkt-new].
    Many thanks and kudos to [@mikearnaldi]
    
    [mikearnaldi/hkt-new]: https://github.com/mikearnaldi/hkt-new
    [@mikearnaldi]: @https://github.com/mikearnaldi
    
    Signed-off-by: Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>

---
+++ src/hkt.ts
@@ -0,0 +1,36 @@
+/* eslint-disable functional/no-return-void,eslint-comments/disable-enable-pair */
+declare const URI: unique symbol
+
+export interface Typeclass<F extends HKT> {
+  readonly [URI]?: F
+}
+
+export interface HKT {
+  /** A = Output (Covariant) */
+  readonly A?: unknown
+  /** E = Error (Covariant) */
+  readonly E?: unknown
+  /** R = Requirements (Contravariant) */
+  readonly R?: unknown
+
+  /** TODO: still to implement S = State (Invariant) I = Category Input (Contravariant) */
+  /** Will represent the computed type */
+  readonly type?: unknown
+}
+
+export type Kind<F extends HKT, R, E, A> = F extends { readonly type: unknown }
+  ? (F /** F has a type specified, it is concrete (like F = ArrayHKT) */ & {
+      readonly R: R
+      readonly E: E
+      readonly A: A
+    })['type']
+  : {
+      /**
+       * F is generic, we need to mention all ot the types parameters to
+       * guarantee that tye are never excluded from the type checking
+       */
+      readonly _F: F
+      readonly _R: (_: R) => void
+      readonly _E: () => E
+      readonly _A: () => A
+    }
